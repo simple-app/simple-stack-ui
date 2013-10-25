c833736401ca3f42beb47ffb08d171be47ec5622
Cameron Bytheway <cameron@theflokk.com>
2013-10-25 12:24:00 -0600

    update the template project

--- files/app.js
+++ files/app.js
@@ -4,13 +4,23 @@
 
 var stack = require('simple-stack-ui');
 var envs = require('envs');
+var routes = require('./public/javascripts/routes');
+
+/**
+ * Expose the app
+ */
 
 var app = module.exports = stack({
   restricted: false,
-  routes: require('./public/javascripts/routes')
+  routes: routes
 });
 
+/**
+ * Setup app-wide locals
+ */
+
 app.locals({
+  app: 'PROJECT',
   env: {
     API_URL: '/api'
   }
--- files/public/javascripts/config.js
+++ files/public/javascripts/config.js
@@ -5,6 +5,29 @@
 var app = require('.');
 var routes = require('./routes');
 
+/**
+ * Initialize aux partials
+ */
+
+loadPartial('header');
+loadPartial('sidenav');
+loadPartial('footer');
+
+/**
+ * Start the app
+ */
+
 module.exports = require('simple-ui').run(app, {
-  routes: routes
+  routes: routes,
+  loader: loadPartial
 });
+
+/**
+ * Helper function for resolving partial paths
+ *
+ * @api private
+ */
+
+function loadPartial(name) {
+  return require('../partials/' + name);
+}
